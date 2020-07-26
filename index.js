76b02958e179e41b8f6afe71555015b0ad09970c
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-07-26 16:19:17 +0200

    Change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 const { data } = require('./data');
 
