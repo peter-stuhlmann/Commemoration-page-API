ed207dd4ea7a19b09df7d0eb404cf85166fffb0d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-09-16 16:16:20 +0200

    Set useCreateIndex to true to prevent a mongoose deprecation warning
    Warning was: "collection.ensureIndex is deprecated. Use createIndexes instead."

--- db/db.js
+++ db/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
+
 module.exports = function connectMongo() {
   mongoose.connect(
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster.dcjud.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
